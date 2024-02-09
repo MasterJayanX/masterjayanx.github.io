6ae3d1cbe753e8574fc6ccc340d8a000224a30f5
MasterJayanX <57007976+MasterJayanX@users.noreply.github.com>
2024-02-09 18:07:05 -0300

    Mejoras

---
+++ kofi.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('masterjayanx', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Apóyame',
+    'floating-chat.donateButton.background-color': '#00b9fe',
+    'floating-chat.donateButton.text-color': '#fff'
+    });
